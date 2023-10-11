9f3437fe94a4792298bbd93006b83f6cfeb1036c
RomanovASC <89694887+RomanovASC@users.noreply.github.com>
2023-10-11 20:28:08 +0400

    Add files via upload

---
+++ 103.js
@@ -0,0 +1,69 @@
+let op;
+let op1 = '';
+let result = 0;
+let a;
+
+function opp() {
+    a = Number(document.getElementById('input').value);
+    document.getElementById ('input').value = '';
+    switch (op1) {
+        case '/':
+            result = result / a;
+            console.log(result);
+            break;
+        case '*':
+            result = result * a;
+            console.log(result);
+            break;
+        case '+':
+            result = result + a;
+            console.log(result);
+            break;
+        case '-':
+            result = result - a;
+            console.log(result);
+            break;
+        case '':
+            result = a;
+            console.log(result);
+            break;
+    }
+    op1 = op;
+}
+
+function calk() {
+    a = Number(document.getElementById('input').value);
+    document.getElementById ('input').value = '';
+    switch (op1) {
+        case '/':
+            result = result / a;
+            console.log(result);
+            break;
+        case '*':
+            result = result * a;
+            console.log(result);
+            break;
+        case '+':
+            result = result + a;
+            console.log(result);
+            break;
+        case '-':
+            result = result - a;
+            console.log(result);
+            break;
+        case '':
+            result = a;
+            console.log(result);
+            break;
+    }
+    
+    let parent = document.querySelector('#output');
+    let p = document.createElement('p');
+    p.textContent = String(result);
+    parent.append(p);
+
+parent.append(p);
+//    document.getElementById ('output').value = result;
+    result = 0;
+    op1 = '';
+}
