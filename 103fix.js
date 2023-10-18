d64e35b482d1a5ba5f55eb0ea503b3ed3a2fcc22
RomanovASC <89694887+RomanovASC@users.noreply.github.com>
2023-10-18 17:53:43 +0400

    Add files via upload

---
+++ 103fix.js
@@ -0,0 +1,41 @@
+let op;
+let op1 = '';
+let result = 0;
+let a;
+
+function opp() {
+    a = Number(document.getElementById('input').value);
+    document.getElementById ('input').value = '';
+    switch (op1) {
+        case '/':
+            result = result / a;
+            console.log(result);
+            break;
+        case '*':
+            result = result * a;
+            console.log(result);
+            break;
+        case '+':
+            result = result + a;
+            console.log(result);
+            break;
+        case '-':
+            result = result - a;
+            console.log(result);
+            break;
+        case '':
+            result = a;
+            console.log(result);
+            break;
+    }
+    op1 = op;
+    if (op1 == "=") {
+        let parent = document.querySelector('#output');
+        let p = document.createElement('p');
+        p.textContent = String(result);
+        parent.append(p);
+        result = 0;
+        op1 = '';
+    }
+
+}
